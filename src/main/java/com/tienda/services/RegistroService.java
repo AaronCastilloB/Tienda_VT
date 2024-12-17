5857b2dc575c339108269eb756957fffb7ada01c
AaronCastilloB <123904213+AaronCastilloB@users.noreply.github.com>
2024-12-17 17:02:04 -0600

    Semana N°11

---
+++ src/main/java/com/tienda/services/RegistroService.java
@@ -0,0 +1,15 @@
+package com.tienda.services;
+
+import com.tienda.domain.Usuario;
+import jakarta.mail.MessagingException;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface RegistroService {
+
+    public Model crear(Model model, Usuario usuario) throws MessagingException;
+
+    public Model activar(Model model, String username, String password);
+
+    public void habilitar(Usuario usuario, MultipartFile imagenFile);
+}
---
+++ src/main/java/com/tienda/services/impl/RegistroServiceImpl.java
@@ -0,0 +1,105 @@
+package com.tienda.services.impl;
+
+import com.tienda.domain.Usuario;
+import com.tienda.service.UsuarioService;
+import com.tienda.services.CorreoService;
+import com.tienda.services.FirebaseStorageService;
+import com.tienda.services.RegistroService;
+import jakarta.mail.MessagingException;
+import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class RegistroServiceImpl implements RegistroService {
+    
+    @Autowired
+    private UsuarioService usuarioService;
+    
+    @Override
+    public Model crear(Model model, Usuario usuario) throws MessagingException {
+        String titulo;
+        String mensaje = null;
+        
+        if (!usuarioService.existeUsuarioPorUsernameOCorreo(usuario.getUsername(),
+                usuario.getCorreo())) {
+
+            //No existe el usuario.... se va a enviar un correo..
+            usuario.setActivo(false);
+            usuario.setPassword(demeClave());
+            usuarioService.save(usuario, false);
+            enviarCorreo(usuario);
+            titulo = mensajes.getMessage("registro.activar", null, Locale.getDefault());
+            mensaje = mensajes.getMessage("registro.mensaje.activacion.ok", null, Locale.getDefault());
+            mensaje = String.format(mensaje, usuario.getCorreo());
+            
+        } else {
+            //Si existe el usuario... se le notifica al usuario...
+            titulo = mensajes.getMessage("registro.activar.error", null, Locale.getDefault());
+            mensaje = mensajes.getMessage("registro.mensaje.usuario.o.correo", null, Locale.getDefault());
+            mensaje = String.format(mensaje, usuario.getUsername(), usuario.getCorreo());
+        }
+        model.addAttribute("titulo", titulo);
+        model.addAttribute("mensaje", mensaje);
+        
+        return model;
+    }
+    
+    @Override
+    public Model activar(Model model, String username, String password) {
+        Usuario usuario = usuarioService.getUsuarioPorUsernameYPassword(username, password);
+        if (usuario != null) {
+            
+            model.addAttribute("usuario", usuario);
+        } else {
+            model.addAttribute("titulo", "Error");
+            model.addAttribute("mensaje", "Error activando usuario");
+        }
+        return model;
+    }
+    
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+    
+    @Override
+    public void habilitar(Usuario usuario, MultipartFile imagenFile) {
+        var encriptador = new BCryptPasswordEncoder();
+        usuario.setPassword(encriptador.encode(usuario.getPassword()));
+        
+        if (!imagenFile.isEmpty()) {
+            String ruta = firebaseStorageService.cargaImagen(imagenFile, "usuarios", usuario.getIdUsuario());
+            usuario.setRutaImagen(ruta);
+        }
+        usuarioService.save(usuario, true);
+    }
+    
+    private String demeClave() {
+        String base = "ABCDEFGHIJKLMNOPQRSTUVXYZ0123456789";
+        String clave = "";
+        for (int i = 0; i < 40; i++) {
+            clave += base.charAt((int) (Math.random() * base.length()));
+        }
+        return clave;
+    }
+    
+    @Autowired
+    private MessageSource mensajes;
+    @Autowired
+    private CorreoService correoService;
+    
+    private void enviarCorreo(Usuario usuario) throws MessagingException {
+        String asunto = mensajes.getMessage("registro.mensaje.activacion", null, Locale.getDefault());
+        String mensaje = mensajes.getMessage("registro.correo.activar", null, Locale.getDefault());
+        mensaje = String.format(mensaje,
+                usuario.getNombre(),
+                usuario.getApellidos(),
+                "localhost",
+                usuario.getUsername(),
+                usuario.getPassword());
+        correoService.enviarCorreoHTML(usuario.getCorreo(), asunto, mensaje);
+    }
+}
